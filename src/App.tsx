766637a4f59421f00fdf106b8019c5ed4bb207fd
vadim-sobinin <vadimsobinin@yandex.ru>
2023-06-16 00:18:02 +0300

    add: complete game logic

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,20 @@ import React, { useEffect, useState } from 'react';
 import './App.scss';
 import { BoardComponent } from './components/BoardComponent';
 import { Board } from './models/Board';
+import { Player } from './models/Player';
+import { Colors } from './models/Colors';
+import LostFigures from './components/LostFigures';
+import Timer from './components/Timer';
 
 function App() {
   const [board, setBoard] = useState(new Board());
+  const [whitePlayer, setWhitePlayer] = useState(new Player(Colors.WHITE));
+  const [blackPlayer, setBlackPlayer] = useState(new Player(Colors.BLACK));
+  const [currentPlayer, setCurrentPlayer] = useState<Player | null>(null);
 
   useEffect(() => {
     restart();
+    setCurrentPlayer(whitePlayer);
   }, []);
 
   const restart = () => {
@@ -18,9 +26,23 @@ function App() {
     setBoard(newBoard);
   };
 
+  function swapPlayer() {
+    setCurrentPlayer(currentPlayer?.color === Colors.WHITE ? blackPlayer : whitePlayer);
+  }
+
   return (
     <div className="app">
-      <BoardComponent board={board} setBoard={setBoard} />
+      <Timer restart={restart} currentPlayer={currentPlayer} />
+      <BoardComponent
+        board={board}
+        setBoard={setBoard}
+        currentPlayer={currentPlayer}
+        swapPlayer={swapPlayer}
+      />
+      <div>
+        <LostFigures title="Черные фигуры" figures={board.lostBlackFigures} />
+        <LostFigures title="Белые фигуры" figures={board.lostWhiteFigures} />
+      </div>
     </div>
   );
 }
--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -3,12 +3,18 @@ import { Cell } from '../models/Cell';
 
 interface CellProps {
   cell: Cell;
+  selected: boolean;
+  click: (cell: Cell) => void;
 }
 
-export const CellComponent: FC<CellProps> = ({ cell }) => {
+export const CellComponent: FC<CellProps> = ({ cell, selected, click }) => {
   return (
-    <div className={`cell ${cell.color}`}>
-      {cell.figure?.logo && <img src={cell.figure?.logo} alt={cell.figure.name} />}
+    <div
+      className={['cell', cell.color, selected ? 'selected' : ''].join(' ')}
+      onClick={() => click(cell)}
+      style={{ background: cell.available && cell.figure ? 'green' : '' }}>
+      {cell.available && !cell.figure && <div className={'available'} />}
+      {cell.figure?.logo && <img src={cell.figure.logo} alt="" />}
     </div>
   );
 };
